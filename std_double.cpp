4e773d7d08c3557b6402106a103fd417ee4d6a83
chenyushuo <297086016@qq.com>
2017-10-02 20:53:31 +0800

    v2.1 add some checkers

---
+++ std_double.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+double a,b;
+char ch;
+typedef unsigned long long uint64;
+void write(const uint64 &x){
+	putchar((x >> 63) & 1 ? '1' : '0');
+	putchar(' ');
+	for (int i=62;i>=52;i--)
+		putchar((x >> i) & 1 ? '1' : '0');
+	putchar(' ');
+	for (int i=51;i>=0;i--)
+		putchar((x >> i) & 1 ? '1' : '0');
+	putchar('\n');
+	//printf("%llu\n",x);
+}
+void set_digit(uint64 *x,int p,int v){
+	assert(v==0||v==1);
+	//if (v) cout<<p<<endl;
+	*x &= ((~0uLL)^(1LL<<p));
+	*x |= ((1LL*v)<<p);
+}
+void set_double(uint64 *x,const char *s){
+	for (int i = 0;i < 64;i++)
+		set_digit(x,i,s[64-i-1] - '0');
+}
+int main(){
+	//scanf("%lf%c%lf",&a,&ch,&b);
+	double res;
+	uint64 *x=(uint64 *)(&res);
+	scanf("%lf",&res);
+	//write(*x);
+	//char s[70];
+	//scanf("%s",s);
+	//set_double(x,s);
+	//set_double(x,string("0000000010001000000000000000000000000000000000000000000000000000").c_str());
+	//write(*x);
+	printf("%.1200f\n",res);
+	//printf("%e\n",res);
+	switch (ch){
+		case '+':
+			printf("%f\n",a+b);
+			res=a+b;
+			write(*x);
+			printf("%f\n",res);
+			break;
+		case '-':
+			printf("%f\n",a-b);
+			res=a-b;
+			write(*x);
+			printf("%f\n",res);
+			break;
+		case '*':
+			printf("%f\n",a*b);
+			res=a*b;
+			write(*x);
+			printf("%f\n",res);
+			break;
+		case '/':
+			printf("%f\n",a/b);
+			res=a/b;
+			write(*x);
+			printf("%f\n",res);
+			break;
+	}
+	return 0;
+}
